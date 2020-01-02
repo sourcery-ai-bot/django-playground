975993d5892a79512c00a9f7b0b9d292ceb86ffb
Allan Lima <allan.lima@ilhasoft.com.br>
2020-01-02 00:32:05 -0300

    Add error messages to person validators

--- playground/core/validators.py
+++ playground/core/validators.py
@@ -4,10 +4,16 @@ from django.core.validators import RegexValidator
 
 
 def validate_document(document):
-    validator = RegexValidator(regex=re.compile(r"^\d{3}\.\d{3}\.\d{3}-\d{2}$"))
+    validator = RegexValidator(
+        regex=re.compile(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$'),
+        message='The document must be in the format xxx.xxx.xxx-xx, where "x" is a digit.'
+    )
     validator(document)
 
 
 def validate_phone(phone):
-    validator = RegexValidator(regex=re.compile("^d{11}$"))
+    validator = RegexValidator(
+        regex=re.compile('^d{11}$'),
+        message='The phone must contain 11 digits only.'
+    )
     validator(phone)
